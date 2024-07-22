ea8b66651e67ed085ac0c6c3fa5e9e28a222fada
aop870325 <pene@163.com>
2024-07-22 18:28:17 +0800

    Update _worker.js
    修改UUID

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'd9670541-9a97-409e-b8c4-7d4835d02abb';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
